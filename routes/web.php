0aa9af8de6ec6f59c9d180c699e0318974268619
Recky Anderson <reckyanderson@gmail.com>
2023-11-28 22:04:10 +0700

    add routes. vision,mission,iku

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,13 @@ Route::middleware(['auth'])->prefix('admin')->group(function () {
         return view('app.dashboard', ['title' => 'Dasbor']);
     })->name('admin.dashboard');
     Route::prefix('renstra')->group(function () {
-        Route::get('visi', [RenstraController::class, 'visi'])->name('renstra.visi');
+        Route::get('visi', [RenstraController::class, 'vision'])->name('vision.index');
+        Route::patch('edit-visi', [RenstraController::class, 'updateVision'])->name('vision.update');
+        Route::get('misi', [RenstraController::class, 'mission'])->name('mission.index');
+        Route::post('tambah-misi', [RenstraController::class, 'storeMission'])->name('mission.store');
+        Route::post('hapus-misi', [RenstraController::class, 'deleteMission'])->name('mission.delete');
+        Route::get('iku', [RenstraController::class, 'iku'])->name('iku.index');
+        Route::post('tambah-iku', [RenstraController::class, 'storeIku'])->name('iku.store');
+        Route::post('hapus-iku', [RenstraController::class, 'deleteIku'])->name('iku.delete');
     });
 });
