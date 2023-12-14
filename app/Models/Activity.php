fdde2400f3cbc88f16271128011b434d0c2b62df
Recky Anderson <reckyanderson@gmail.com>
2023-12-14 14:47:45 +0700

    add calculatetotalsum and activity recap

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -15,4 +15,30 @@ class Activity extends Model
     {
         return $this->hasMany(BudgetImplementation::class);
     }
+
+    /**
+     * Calculate the total sum of a specific field in BudgetImplementation.
+     *
+     * @return float
+     */
+    public function calculateTotalSum()
+    {
+        return $this->budgetImplementations->sum(function ($budgetImplementation) {
+            return $budgetImplementation->details->sum('total');
+        });
+    }
+    public function activityRecap()
+    {
+        return $this->hasOne(ActivityRecap::class);
+    }
+    /**
+     * Scope a query to sort activities by code in a case-sensitive manner.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeSortedByCode($query)
+    {
+        return $query->orderBy('code');
+    }
 }
