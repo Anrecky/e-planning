f147749a25f95b7b9e33a0344fb5d18d70c8a0c7
Recky Anderson <reckyanderson@gmail.com>
2023-12-29 10:00:43 +0700

    add functions format to IDR and enforceNumeric

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,26 @@ import.meta.glob([
     '../images/**',
 ]);
 import './import.plugins'
+
+
+function formatAsIDRCurrency(value) {
+    if (!isNaN(value)) {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0
+    }).format(value);
+    }
+    return '';
+}
+
+function enforceNumericInput(event) {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        event.preventDefault();
+    }
+}
+
+window.formatAsIDRCurrency = formatAsIDRCurrency;
+window.enforceNumericInput = enforceNumericInput;
