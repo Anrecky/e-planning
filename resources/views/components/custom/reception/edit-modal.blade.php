e225e6d53917adc15930b436b071a65e7887e423
Recky Anderson <reckyanderson@gmail.com>
2024-01-13 20:22:32 +0700

    fix edit modal

--- resources/views/components/custom/reception/edit-modal.blade.php
+++ resources/views/components/custom/reception/edit-modal.blade.php
@@ -8,10 +8,47 @@
                         data-feather="x-circle" class="feather-24"></i></button>
             </div>
             <div class="modal-body">
-                <form id="form-edit" action="{{ route('budget_implementation.update') }}" method="POST">
+                <form id="form-edit" action="" method="POST">
                     @csrf
                     @method('PATCH')
-                    <div id="edit-input_container" class="input-group my-2">
+
+                    <div class="mb-3 row">
+                        <label for="accountCode" class="col-sm-3 col-form-label">Kode Akun</label>
+                        <div class="col-sm-7">
+                            <select name="accountCode" id="editSelectAccountCode" class="form-control"></select>
+                        </div>
+                    </div>
+                    <div class="mb row">
+                        <label for="description" class="col-sm-3 col-form-label">Uraian</label>
+                        <div class="col-sm-9">
+                            <textarea class="form-control" name="description" id="description" rows="2"></textarea>
+                        </div>
+                    </div>
+                    <div class="mb-3 row align-items-center">
+                        <p class="col-sm-3 col-form-label text-dark">Jenis</p>
+                        <div class="col-sm-7">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="checkbox" checked name="type[]"
+                                    id="umum-checkbox" value="umum">
+                                <label class="form-check-label mb-0" for="umum-checkbox">Umum</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="checkbox" name="type[]" id="fungsional-checkbox"
+                                    value="fungsional">
+                                <label class="form-check-label mb-0" for="fungsional-checkbox">Fungsional</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="checkbox" name="type[]" id="pajak-checkbox"
+                                    value="pajak">
+                                <label class="form-check-label mb-0" for="pajak-checkbox">Pajak</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="target" class="col-sm-3 col-form-label">Target Pendapatan</label>
+                        <div class="col-sm-6">
+                            <input type="text" class="form-control" id="editTarget" name="target">
+                        </div>
                     </div>
                     <button class="btn btn-primary text-center align-items-center mt-1 mt-2 py-auto" type="submit">
                         <span class="icon-name">Simpan</span>
