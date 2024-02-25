64bfcf679627d469af2485c5ed2f4bf360690002
Recky Anderson <reckyanderson@gmail.com>
2024-02-25 20:39:00 +0700

    add ppks,treasurers,activities data to the view

--- app/Http/Controllers/PaymentReceiptController.php
+++ app/Http/Controllers/PaymentReceiptController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Activity;
+use App\Models\PPK;
+use App\Models\Treasurer;
 use Illuminate\Http\Request;
 
 class PaymentReceiptController extends Controller
@@ -9,7 +12,10 @@ class PaymentReceiptController extends Controller
     public function index()
     {
         $title = 'Rekam Kuitansi';
+        $ppks = PPK::all();
+        $treasurers = Treasurer::all();
+        $activities = Activity::all();
 
-        return view('app.payment-receipt', compact('title'));
+        return view('app.payment-receipt', compact('title', 'ppks', 'treasurers', 'activities'));
     }
 }
