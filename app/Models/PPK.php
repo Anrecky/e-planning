1a356f58f7cca2418fca92c6a8bef0237a5d4838
Recky Anderson <reckyanderson@gmail.com>
2024-02-25 20:39:58 +0700

    bind the factory method using newfactory

--- app/Models/PPK.php
+++ app/Models/PPK.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Database\Factories\PPKFactory;
 
 class PPK extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'nik', 'position'];
+
+    /**
+     * Create a new factory instance for the model.
+     */
+    protected static function newFactory(): Factory
+    {
+        return PPKFactory::new();
+    }
 }
