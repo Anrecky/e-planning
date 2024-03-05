ddc2b0f3881d2daa062438b1ef5c8570d12ec9cb
Recky Anderson <reckyanderson@gmail.com>
2024-03-05 23:38:03 +0700

    finish payment verification feature

---
+++ app/Models/PaymentVerification.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class PaymentVerification extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'description',
+        'activity_date',
+        'amount',
+        'provider',
+        'implementer_name',
+        'implementer_nip',
+        'auditor_name',
+        'auditor_nip',
+        'ppk_id',
+        'verificator_id'
+    ];
+    public function ppk(): BelongsTo
+    {
+        return $this->belongsTo(PPK::class);
+    }
+    public function verificator(): BelongsTo
+    {
+        return $this->belongsTo(Verificator::class);
+    }
+}
---
+++ database/migrations/2024_03_03_195132_create_payment_verifications_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\Verificator;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_verifications', function (Blueprint $table) {
+            $table->id();
+            $table->text('description');
+            $table->date('activity_date')->nullable();
+            $table->decimal('amount', 15, 2)->default(0);
+            $table->string('provider');
+            $table->string('implementer_name');
+            $table->string('implementer_nip');
+            $table->string('auditor_name');
+            $table->string('auditor_nip');
+            $table->foreignId('ppk_id')
+                ->constrained('ppks')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignIdFor(Verificator::class)
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_verifications');
+    }
+};
