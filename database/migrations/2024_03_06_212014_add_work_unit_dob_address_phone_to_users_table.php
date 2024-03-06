582ce92ad2bf2746e3095cb5e2d52b803b73e54b
Recky Anderson <reckyanderson@gmail.com>
2024-03-06 21:23:34 +0700

    add work_unit,dob,address,phone to users table

---
+++ database/migrations/2024_03_06_212014_add_work_unit_dob_address_phone_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\WorkUnit;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignIdFor(WorkUnit::class)
+                ->nullable()
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->date('dob')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
