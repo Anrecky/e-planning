3c867e4b025d09bba6e4ae95b376e2e7c1ec7e17
Recky Anderson <reckyanderson@gmail.com>
2024-03-13 03:19:01 +0700

    add nullable on identity number

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->bigInteger('identity_number');
+            $table->integer('identity_number')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
