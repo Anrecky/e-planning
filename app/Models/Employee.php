722bfaa8afdf6a83ceedc801538252f35c2f856b
Ugik DEV <ugik.dev@gmail.com>
2024-03-18 18:45:47 +0700

    add staff_id model Employee

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Employee extends Model
 {
@@ -15,10 +16,16 @@ class Employee extends Model
         'position',
         'work_unit_id',
         'user_id',
+        'staff_id',
     ];
 
     public function user(): HasOne
     {
         return $this->hasOne(User::class);
+    }
+
+    public function staff(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'staff_id', 'id');
     }
 }
